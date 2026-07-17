d7c135974f8dd02c0158aaba88692b8e5cb2ad3b
fealafoo <alafoo.feras@outlook.com>
2026-07-17 15:05:46 +0300

    a

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pool
 
 go 1.25.0
+
+require github.com/01-edu/z01 v0.1.0 // indirect
--- level5/fprime.go
+++ level5/fprime.go
@@ -1,24 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	args := os.Args
-	fmt.Print(args[0])
-	z01.PrintRune(1)
-	z01.PrintRune('a')
+	if len(os.Args) != 2 {
+		return
+	}
+	arg := os.Args[1]
+	n, err := strconv.Atoi(arg)
+	if err != nil {
+		return
+	}
+	if n < 2 {
+		return
+	}
+
+	for i := 2; i <= n; i++ {
+		for n%i == 0 {
+			if isPrime(i) {
+				toPrint := i
+				x := ""
+				for toPrint > 0 {
+					x = string(rune(toPrint%10 + '0')) + x
+					toPrint /= 10
+				}
+				for i := 0; i < len(x); i++ {
+					z01.PrintRune(rune(x[i]))
+				}
+				if i < n {
+					z01.PrintRune('*')
+				}
+				n /= i
+			}
+		}
+	}
+	z01.PrintRune('\n')
 }
 
-func isPrime(n int, divider int) bool {
+func isPrime(n int) bool {
 	if n < 2 {
 		return false
 	}
-	for i := divider; i*i >= n; i++ {
+	if n == 2 {
+		return true
+	}
+	for i := 2; i*i <= n; i++ {
 		if n%i == 0 {
 			return false
 		}
